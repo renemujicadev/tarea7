42e276fb6d09818c4f4bb39f4dd613bc2e7c5130
René Mujica Moreau <rene.mujica.m@gmail.com>
2020-06-09 21:38:30 -0400

    logs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import boto3
 import datetime
+import time
 
 def parseText(textDetections):
     detectedText = ""
@@ -21,13 +22,23 @@ if __name__ == "__main__":
   
     controlDetection=client.detect_text(Image={'S3Object':{'Bucket':bucket,'Name':control}})
     controlText = parseText(controlDetection['TextDetections'])
+    logs = []
 
     for i in range(1, 11):
         print("Test ID:", i)
+        logs.append("Test ID: "+ str(i))
         print("Hora de la prueba:",datetime.datetime.now())
+        logs.append("Hora de la prueba: "+str(datetime.datetime.now()))
         testDetection = client.detect_text(Image={'S3Object':{'Bucket':bucket,'Name':photo.format(i)}})
         detectedText = parseText(testDetection['TextDetections'])
-        print("Texto detectado:", detectedText.lower())
+        print("Texto detectado: ", detectedText.lower())
+        logs.append("Texto detectado: "+ detectedText.lower())
         print("iguales?", controlText.lower().strip() == detectedText.lower().strip())
+        logs.append("iguales? "+ str(controlText.lower().strip() == detectedText.lower().strip()))
         print("====")
+        logs.append("====")
+
+    with open("logs-"+str(time.time())+".txt", "w") as f:
+        for log in logs:
+            f.write("%s\n" % log)
     
